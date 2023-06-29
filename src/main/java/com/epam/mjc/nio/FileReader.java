4deed2ff8fd51628a612ada67981ba6da21b9773
victor diaz <vdmpeniel@yahoo.es>
2023-06-29 16:34:28 -0400

    Added all required changes

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,76 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 
 public class FileReader {
+    public String readFile(File file){
+        StringBuilder content = new StringBuilder();
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r");
+             FileChannel inChannel = aFile.getChannel();) {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+            //Buffer size is 1024
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    content.append((char) buffer.get());
+                }
+                buffer.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
     }
+
+
+    private void populateField(Profile profile, String field, String value){
+        try {
+            switch (field) {
+                case "Name":
+                    profile.setName(value);
+                    break;
+                case "Age":
+                    profile.setAge(Integer.valueOf(value));
+                    break;
+                case "Email":
+                    profile.setEmail(value);
+                    break;
+                case "Phone":
+                    profile.setPhone(Long.valueOf(value));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Field " + field + "is not allowed.");
+            }
+        } catch(IllegalArgumentException iae){
+            iae.printStackTrace();
+        }
+    }
+    private String[] fields = {"Name", "Age", "Email", "Phone"};
+    private Profile parseContent(String content){
+        String[] lines = content.split("\r?\n");
+
+        Profile profile = new Profile();
+        for(String str : lines){
+            for(String field : fields){
+                if(str.contains(field)){
+                    populateField(profile, field, str.replace(field + ": ", ""));
+                }
+            }
+        }
+        return profile;
+    }
+    public Profile getDataFromFile(File file) {
+        return parseContent(readFile(file));
+    }
+
+
 }
